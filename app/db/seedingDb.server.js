658d657f59e4dd4cb8a4767851a198015d5ca82c
Søs Wind <s_swind@hotmail.com>
2024-03-12 10:20:00 +0100

    Adgangskoder ændret i seedingDb til de rigtige koder

--- app/db/seedingDb.server.js
+++ app/db/seedingDb.server.js
@@ -10,37 +10,37 @@ export default async function seedDb() {
   }
 }
 
-async function insertData() {
+async function insertUsers() {
   const users = [
     {
       username: "Søren Madsen",
       email: "madsen@mail.dk",
-      password: await bcrypt.hash("password", 10),
+      password: await bcrypt.hash("1234", 10),
     },
     {
       username: "Mie Hansen",
       email: "mie@mail.dk",
-      password: await bcrypt.hash("password", 10),
+      password: await bcrypt.hash("Mie123", 10),
     },
     {
       username: "Martin Hansen",
       email: "martin@mail.dk",
-      password: await bcrypt.hash("password", 10),
+      password: await bcrypt.hash("1234", 10),
     },
     {
       username: "Søs Wind",
       email: "soswind@mail.dk",
-      password: await bcrypt.hash("password", 10),
+      password: await bcrypt.hash("Hihi123", 10),
     },
     {
       username: "Hanne Andersen",
       email: "hanne@mail.dk",
-      password: await bcrypt.hash("password", 10),
+      password: await bcrypt.hash("Hanne123", 10),
     },
     {
       username: "Maj Jensen",
       email: "maj@mail.dk",
-      password: await bcrypt.hash("password", 10),
+      password: await bcrypt.hash("Maj123", 10),
     },
 
   ];
